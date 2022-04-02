db420d98585f3a4dd5f521cb29f4285ca93be9e2
Szymon Bednorz <dsonyyy@gmail.com>
2022-04-02 19:01:33 +0200

    basic structs

---
+++ src/structs.go
@@ -0,0 +1,31 @@
+package main
+
+type IDType uint
+
+type User struct {
+	ID   IDType `json:"id"`
+	Name string `json:"name"`
+}
+
+type SmallTask struct {
+	ID   IDType `json:"id"`
+	Name string `json:"name"`
+}
+
+type ComplexTask struct {
+	ID   IDType `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	ID      IDType `json:"id"`
+	UserID  IDType `json:"user_id"`
+	Conetnt string `json:"content"`
+}
+
+type Room struct {
+	ID           IDType   `json:"id"`
+	Users        []IDType `json:"users"`
+	SmallTasks   []IDType `json:"smallTasks"`
+	ComplexTasks []IDType `json:"complexTasks"`
+}
